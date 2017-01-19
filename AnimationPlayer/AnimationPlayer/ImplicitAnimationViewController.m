0002bb4e26dc8a1c074e47e20256eab24dd398e4
sulirong <15528290768@163.com>
2017-01-19 14:00:34 +0800

    增加一个选项，控制是否允许隐式动画。

--- AnimationPlayer/AnimationPlayer/ImplicitAnimationViewController.m
+++ AnimationPlayer/AnimationPlayer/ImplicitAnimationViewController.m
@@ -10,7 +10,7 @@
 
 @interface ImplicitAnimationViewController ()
 @property (weak, nonatomic) IBOutlet UIView *targetView;
-
+@property (nonatomic) BOOL disableImplicit;
 @end
 
 @implementation ImplicitAnimationViewController
@@ -34,15 +34,31 @@
 }
 
 - (IBAction)changeBgColor:(id)sender {
+    if (self.disableImplicit) {
+        [CATransaction begin];
+        [CATransaction setValue:(id)kCFBooleanTrue
+                         forKey:kCATransactionDisableActions];
+    }
+    
     if (CGColorEqualToColor(_subLayer.backgroundColor, [UIColor cyanColor].CGColor)) {
         _subLayer.backgroundColor = [UIColor yellowColor].CGColor;
     }
     else {
         _subLayer.backgroundColor = [UIColor cyanColor].CGColor;
     }
+    
+    if (self.disableImplicit) {
+        [CATransaction commit];
+    }
 }
 
 - (IBAction)changeOpacity:(id)sender {
+    if (self.disableImplicit) {
+        [CATransaction begin];
+        [CATransaction setValue:(id)kCFBooleanTrue
+                         forKey:kCATransactionDisableActions];
+    }
+    
     CGFloat startOpacity = 0;
     CGFloat targetOpacity = 0;
     if (_subLayer.opacity < 0.4) {
@@ -55,43 +71,72 @@
     }
     
     _subLayer.opacity = targetOpacity;
+    
+    if (self.disableImplicit) {
+        [CATransaction commit];
+    }
 }
 
 - (IBAction)changePosition:(id)sender {
+    if (self.disableImplicit) {
+        [CATransaction begin];
+        [CATransaction setValue:(id)kCFBooleanTrue
+                         forKey:kCATransactionDisableActions];
+    }
+    
     if (_subLayer.position.x > self.targetView.bounds.size.width / 2) {
         _subLayer.position = CGPointMake(self.targetView.bounds.size.width/2, self.targetView.bounds.size.height/2);
     }
     else {
         _subLayer.position = CGPointMake(self.targetView.bounds.size.width/2+10, self.targetView.bounds.size.height/2+10);
     }
+    
+    if (self.disableImplicit) {
+        [CATransaction commit];
+    }
 }
 
 - (IBAction)changeSize:(id)sender {
+    if (self.disableImplicit) {
+        [CATransaction begin];
+        [CATransaction setValue:(id)kCFBooleanTrue
+                         forKey:kCATransactionDisableActions];
+    }
+    
     if (_subLayer.bounds.size.width < self.targetView.bounds.size.width - 10) {
         _subLayer.bounds = CGRectMake(0, 0, self.targetView.bounds.size.width - 10, self.targetView.bounds.size.height - 10);
     }
     else {
         _subLayer.bounds = CGRectMake(0, 0, self.targetView.bounds.size.width - 30, self.targetView.bounds.size.height - 30);
     }
+    
+    if (self.disableImplicit) {
+        [CATransaction commit];
+    }
 }
 
 - (IBAction)changeAnchorPoint:(id)sender {
+    if (self.disableImplicit) {
+        [CATransaction begin];
+        [CATransaction setValue:(id)kCFBooleanTrue
+                         forKey:kCATransactionDisableActions];
+    }
+    
     if (CGPointEqualToPoint(_subLayer.anchorPoint, CGPointMake(.5, .5))) {
         _subLayer.anchorPoint = CGPointMake(1, .5);
     }
     else {
         _subLayer.anchorPoint = CGPointMake(.5, .5);
     }
+    
+    if (self.disableImplicit) {
+        [CATransaction commit];
+    }
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)switchValueChanged:(id)sender {
+    UISwitch *switchControl = (UISwitch*)sender;
+    self.disableImplicit = switchControl.isOn;
 }
-*/
 
 @end
